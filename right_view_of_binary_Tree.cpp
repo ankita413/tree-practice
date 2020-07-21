2b88b899059a712c6b4138d72cfceac0fb5420c4
ankita413 <55186999+ankita413@users.noreply.github.com>
2020-07-21 15:37:49 +0530

    right_view_of_binary_Tree.cpp

---
+++ right_view_of_binary_Tree.cpp
@@ -0,0 +1,86 @@
+//iterative approach
+void rightView_helper(Node *root,int level,int*maxlevel)
+{
+    if(root == NULL)
+        return;
+        if(*maxlevel < level)
+        {
+            cout<<root -> data<<" ";
+            *maxlevel = level;
+        }
+        rightView_helper(root->right, level + 1,maxlevel);
+        rightView_helper(root->left, level + 1, maxlevel);
+}
+
+void rightView(Node *root)
+{
+int max_level = 0;
+rightView_helper(root,1,&max_level);
+}
+
+//recursive approach
+void rightView(struct Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
